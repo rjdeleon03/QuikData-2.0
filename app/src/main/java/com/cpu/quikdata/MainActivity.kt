8468a735752d9f1724ef899cf067399aa00240d6
Pabs <pabs@alink-group.co.jp>
2019-05-24 09:32:10 +0800

    Refine toolbar and navigation layouts

--- app/src/main/java/com/cpu/quikdata/MainActivity.kt
+++ app/src/main/java/com/cpu/quikdata/MainActivity.kt
@@ -2,11 +2,20 @@ package com.cpu.quikdata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mNavController = findNavController(R.id.mainActivityFragment)
+        mainActivityNavigationView.setupWithNavController(mNavController)
     }
 }
