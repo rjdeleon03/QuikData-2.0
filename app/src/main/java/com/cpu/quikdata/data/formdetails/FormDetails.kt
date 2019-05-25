11e088a91bb127331655aa40201c4dc016350803
rjdeleon03 <ricardopablo.deleon@gmail.com>
2019-05-25 23:33:46 +0800

    Create form details when form is created

--- app/src/main/java/com/cpu/quikdata/data/formdetails/FormDetails.kt
+++ app/src/main/java/com/cpu/quikdata/data/formdetails/FormDetails.kt
@@ -1,14 +1,24 @@
 package com.cpu.quikdata.data.formdetails
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.cpu.quikdata.data.form.Form
 
-@Entity(tableName = "form_details")
+@Entity(tableName = "form_details",
+    indices = [Index("formId")],
+    foreignKeys = [ForeignKey(entity = Form::class,
+        parentColumns = ["id"],
+        childColumns = ["formId"],
+        onDelete = ForeignKey.CASCADE
+    )])
 data class FormDetails(@PrimaryKey(autoGenerate = false)
                        val id: String,
-                       val assessmentDate: Long,
-                       val interviewer: String,
-                       val interviewerContact: String,
-                       val interviewee: String,
-                       val intervieweeContact: String,
-                       val sourcesOfInformation: String)
+                       val assessmentDate: Long = 0L,
+                       val interviewer: String = "",
+                       val interviewerContact: String = "",
+                       val interviewee: String = "",
+                       val intervieweeContact: String = "",
+                       val sourcesOfInformation: String = "",
+                       val formId: String)
--- app/src/main/java/com/cpu/quikdata/feature/main/newforms/NewFormsRepository.kt
+++ app/src/main/java/com/cpu/quikdata/feature/main/newforms/NewFormsRepository.kt
@@ -4,11 +4,16 @@ import android.app.Application
 import androidx.lifecycle.LiveData
 import com.cpu.quikdata.data.AppDatabase
 import com.cpu.quikdata.data.form.Form
+import com.cpu.quikdata.data.formdetails.FormDetails
 import com.cpu.quikdata.utils.generateId
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import org.joda.time.DateTime
+import org.joda.time.LocalDate
+import org.joda.time.LocalDateTime
+import org.joda.time.LocalTime
 
 class NewFormsRepository(application: Application) {
 
@@ -20,8 +25,14 @@ class NewFormsRepository(application: Application) {
 
     fun createNewForm() {
         CoroutineScope(Job() + Dispatchers.Main).launch(Dispatchers.IO) {
-            val form = Form(generateId())
+            val id = generateId()
+            val form = Form(id)
             mDatabase.formDao().insert(form)
+
+            val formDetails = FormDetails(id = generateId(),
+                assessmentDate = DateTime().millis,
+                formId = id)
+            mDatabase.formDetailsDao().insert(formDetails)
         }
     }
 
