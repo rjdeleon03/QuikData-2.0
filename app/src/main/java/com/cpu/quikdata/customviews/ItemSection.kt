ac82df961636a2a942badf3beddb10edf188b38a
rjdeleon <ricardopablo.deleon@gmail.com>
2019-05-25 16:59:54 +0800

    Update create form layout

--- app/src/main/java/com/cpu/quikdata/customviews/ItemSection.kt
+++ app/src/main/java/com/cpu/quikdata/customviews/ItemSection.kt
@@ -5,10 +5,15 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
 import com.cpu.quikdata.R
+import kotlinx.android.synthetic.main.view_item_section.view.*
 
 class ItemSection(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
     init {
         View.inflate(context, R.layout.view_item_section, this)
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.ItemSection)
+        label.text = attributes.getString(R.styleable.ItemSection_text)
+        attributes.recycle()
     }
 }
