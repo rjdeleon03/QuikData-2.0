7359890232c6e78f3318221ecc2c06b2170ae076
rjdeleon03 <ricardopablo.deleon@gmail.com>
2019-05-30 17:27:30 +0800

    Implement saving of infrastructure damage

--- app/src/main/java/com/cpu/quikdata/feature/createform/generalinfo/infrastructuredamage/InfrastructureDamageAdapter.kt
+++ app/src/main/java/com/cpu/quikdata/feature/createform/generalinfo/infrastructuredamage/InfrastructureDamageAdapter.kt
@@ -8,6 +8,7 @@ import com.cpu.quikdata.base.BaseAdapter
 import com.cpu.quikdata.common.InfraCategories
 import com.cpu.quikdata.customviews.CollapsibleContainer
 import com.cpu.quikdata.data.generalinfo.infrastructuredamage.InfrastructureDamageRow
+import kotlinx.android.synthetic.main.item_infrastructure_damage.view.*
 import kotlinx.android.synthetic.main.view_collapsible_container.view.*
 
 class InfrastructureDamageAdapter(context: Context, rowSaveListener: (InfrastructureDamageRow) -> Unit) :
@@ -30,25 +31,23 @@ class InfrastructureDamageAdapter(context: Context, rowSaveListener: (Infrastruc
         override fun populateWithData(row: InfrastructureDamageRow, rowSaveListener: (InfrastructureDamageRow) -> Unit) {
             view.tag = row.id
             view.headerTextField.setText(InfraCategories.getStringId(row.type))
-//            view.populationAffectedText.number1 = row.affectedMale
-//            view.populationAffectedText.number2 = row.affectedFemale
-//            view.populationDisplacedText.number1 = row.displacedMale
-//            view.populationDisplacedText.number2 = row.displacedFemale
+            view.infrastructureDamageNumberText.number = row.numberOfInfrastructure
+            view.infrastructureDamageFunctionalBool.value = row.isFunctional
+            view.infrastructureDamageRemarksText.text = row.remarks
 
-            // Setup listener for saving each population row
+            // Setup listener for saving each infrastructure damage row
             (view as CollapsibleContainer).onDetachedListener = {
-//                val newRow = InfrastructureDamageRow(
-//                    row.id,
-//                    row.type,
-//                    view.populationAffectedText.number1,
-//                    view.populationAffectedText.number2,
-//                    view.populationDisplacedText.number1,
-//                    view.populationDisplacedText.number2,
-//                    row.formId
-//                )
-//                if (row != newRow) {
-//                    rowSaveListener(newRow)
-//                }
+                val newRow = InfrastructureDamageRow(
+                    row.id,
+                    row.type,
+                    view.infrastructureDamageNumberText.number,
+                    view.infrastructureDamageFunctionalBool.value,
+                    view.infrastructureDamageRemarksText.text,
+                    row.formId
+                )
+                if (row != newRow) {
+                    rowSaveListener(newRow)
+                }
             }
 
         }
