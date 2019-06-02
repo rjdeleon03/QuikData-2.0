b3e3e45c23cf9349d85916baf53c93aef51bdbe1
rjdeleon <ricardopablo.deleon@gmail.com>
2019-06-03 07:30:10 +0800

    Add health assistance section

---
+++ app/src/main/java/com/cpu/quikdata/feature/createform/healthinfo/healthassistance/HealthAssistanceRepository.kt
@@ -0,0 +1,37 @@
+package com.cpu.quikdata.feature.createform.healthinfo.healthassistance
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.cpu.quikdata.base.BaseCreatableDataRepository
+import com.cpu.quikdata.data.health.healthassistance.HealthAssistanceRow
+import com.cpu.quikdata.utils.generateId
+import com.cpu.quikdata.utils.runOnIoThread
+import org.joda.time.LocalDate
+import org.joda.time.LocalDateTime
+
+class HealthAssistanceRepository(application: Application, formId: String) :
+    BaseCreatableDataRepository<HealthAssistanceRow>(application) {
+
+    private val mFormId = formId
+    private val mHealthAssistance = mDatabase.healthAssistanceRowDao().getByFormId(formId)
+
+    val healthAssistance: LiveData<List<HealthAssistanceRow>>
+        get() = mHealthAssistance
+
+    override fun updateData(data: HealthAssistanceRow) {
+        runOnIoThread {
+            mDatabase.healthAssistanceRowDao().update(data)
+        }
+    }
+
+    override fun createData() {
+        runOnIoThread {
+            val dateTodayInMillis = LocalDate.now().toDateTimeAtStartOfDay().millis
+            val row = HealthAssistanceRow(id = generateId(),
+                dateReceived = dateTodayInMillis,
+                dateCreated = LocalDateTime.now().toDateTime().millis,
+                formId = mFormId)
+            mDatabase.healthAssistanceRowDao().insert(row)
+        }
+    }
+}
---
+++ app/src/main/java/com/cpu/quikdata/feature/createform/healthinfo/healthassistance/HealthAssistanceViewModel.kt
@@ -0,0 +1,19 @@
+package com.cpu.quikdata.feature.createform.healthinfo.healthassistance
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.cpu.quikdata.data.health.healthassistance.HealthAssistanceRow
+
+class HealthAssistanceViewModel(application: Application, formId: String) :
+    AndroidViewModel(application) {
+
+    private val mRepository = HealthAssistanceRepository(application, formId)
+
+    val healthAssistance: LiveData<List<HealthAssistanceRow>>
+        get() = mRepository.healthAssistance
+
+    fun updateRow(healthAssistanceRow: HealthAssistanceRow) = mRepository.updateData(healthAssistanceRow)
+
+    fun createRow() = mRepository.createData()
+}
