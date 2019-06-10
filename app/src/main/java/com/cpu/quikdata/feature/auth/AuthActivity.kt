dcaff5cfa059db5bdf474dfc27455fe50fb18060
rjdeleon <ricardopablo.deleon@gmail.com>
2019-06-11 07:59:50 +0800

    Add simple authentication screens

---
+++ app/src/main/java/com/cpu/quikdata/feature/auth/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.cpu.quikdata.feature.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cpu.quikdata.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
