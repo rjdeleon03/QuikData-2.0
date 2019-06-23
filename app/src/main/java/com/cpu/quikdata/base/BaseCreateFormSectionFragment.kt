d02c5726733d875b555eafd98e10a0fc70b12a17
rjdeleon <ricardopablo.deleon@gmail.com>
2019-06-23 19:27:45 +0800

    Add DNCA subtitle

--- app/src/main/java/com/cpu/quikdata/base/BaseCreateFormSectionFragment.kt
+++ app/src/main/java/com/cpu/quikdata/base/BaseCreateFormSectionFragment.kt
@@ -1,13 +1,14 @@
 package com.cpu.quikdata.base
 
 import androidx.fragment.app.Fragment
+import com.cpu.quikdata.R
 import com.cpu.quikdata.feature.createform.CreateFormActivity
 
 abstract class BaseCreateFormSectionFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        (activity!! as CreateFormActivity).setSubtitle()
+        (activity!! as CreateFormActivity).setSubtitle(getString(R.string.create_form_subtitle))
     }
 
     protected fun setSubtitle(subtitle: CharSequence?) {
