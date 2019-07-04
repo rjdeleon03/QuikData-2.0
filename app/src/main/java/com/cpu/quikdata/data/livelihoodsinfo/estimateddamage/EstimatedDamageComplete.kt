932ff240c8a44d6afbffbf0580d8cdf70a4b6a75
rjdeleon <ricardopablo.deleon@gmail.com>
2019-07-04 08:45:01 +0800

    Override equality checking for estimated damage complete class

--- app/src/main/java/com/cpu/quikdata/data/livelihoodsinfo/estimateddamage/EstimatedDamageComplete.kt
+++ app/src/main/java/com/cpu/quikdata/data/livelihoodsinfo/estimateddamage/EstimatedDamageComplete.kt
@@ -10,4 +10,24 @@ class EstimatedDamageComplete {
         entityColumn = "estimatedDamageId",
         entity = EstimatedDamageType::class)
     var types: List<EstimatedDamageType>? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is EstimatedDamageComplete) return false
+        var areTypesEqual = false
+        if (this.types == null && other.types == null) {
+            areTypesEqual = true
+        } else if (this.types != null && other.types != null) {
+            if (this.types!!.size != other.types!!.size) areTypesEqual = false
+            else {
+                for (i in 0 until this.types!!.size) {
+                    if (this.types!![i] != other.types!![i]){
+                        areTypesEqual = false
+                        break
+                    }
+                }
+            }
+        }
+
+        return areTypesEqual && row == other
+    }
 }
