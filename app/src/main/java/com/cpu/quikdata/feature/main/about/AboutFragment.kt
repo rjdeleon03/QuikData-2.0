06b4307c4c869a2d6326aec6534fb568f00d129a
rjdeleon <ricardopablo.deleon@gmail.com>
2019-07-21 11:09:20 +0800

    Add about page

---
+++ app/src/main/java/com/cpu/quikdata/feature/main/about/AboutFragment.kt
@@ -0,0 +1,40 @@
+package com.cpu.quikdata.feature.main.about
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.cpu.quikdata.R
+import com.cpu.quikdata.common.addWebLink
+import com.cpu.quikdata.common.clickWithGuard
+import com.cpu.quikdata.common.setupClipping
+import com.cpu.quikdata.feature.consortium.ConsortiumActivity
+import kotlinx.android.synthetic.main.fragment_about.*
+
+class AboutFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = AboutFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClipping(settingsMainLayout)
+
+        aboutICREDButton.addWebLink("https://www.cdrc-phil.com/icred/", context!!)
+        aboutConsortiumButton.clickWithGuard {
+            ConsortiumActivity.newInstance(context!!)
+        }
+    }
+
+}
