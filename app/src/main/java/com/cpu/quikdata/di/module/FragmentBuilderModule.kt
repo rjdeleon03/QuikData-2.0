45dd1a84c7fc3566d023791433615dffc4462016
rjdeleon03 <rjdeleon@up.edu.ph>
2020-02-11 19:32:17 +0800

    Organize files

--- app/src/main/java/com/cpu/quikdata/di/module/FragmentBuilderModule.kt
+++ app/src/main/java/com/cpu/quikdata/di/module/FragmentBuilderModule.kt
@@ -1,7 +1,7 @@
 package com.cpu.quikdata.di.module
 
-import com.cpu.quikdata.di.newforms.HomeViewModelModule
-import com.cpu.quikdata.di.newforms.NewFormsViewModelModule
+import com.cpu.quikdata.di.module.viewmodel.HomeViewModelModule
+import com.cpu.quikdata.di.module.viewmodel.NewFormsViewModelModule
 import com.cpu.quikdata.feature.imageviewer.ImageViewerFragment
 import com.cpu.quikdata.feature.main.home.HomeFragment
 import com.cpu.quikdata.feature.main.newforms.NewFormsFragment
--- app/src/main/java/com/cpu/quikdata/di/module/viewmodel/HomeViewModelModule.kt
+++ app/src/main/java/com/cpu/quikdata/di/module/viewmodel/HomeViewModelModule.kt
@@ -1,22 +1,22 @@
-package com.cpu.quikdata.di.newforms
+package com.cpu.quikdata.di.module.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.cpu.quikdata.data.AppDatabase
 import com.cpu.quikdata.di.annotation.ViewModelKey
+import com.cpu.quikdata.feature.main.home.HomeViewModel
 import com.cpu.quikdata.feature.main.newforms.NewFormsRepository
-import com.cpu.quikdata.feature.main.newforms.NewFormsViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
 
 @Module
-abstract class NewFormsViewModelModule {
+abstract class HomeViewModelModule {
 
     @Binds
     @IntoMap
-    @ViewModelKey(NewFormsViewModel::class)
-    abstract fun bindNewFormsViewModel(viewModel: NewFormsViewModel): ViewModel
+    @ViewModelKey(HomeViewModel::class)
+    abstract fun bindsHomeViewModel(homeViewModel: HomeViewModel): ViewModel
 
     companion object {
         @Provides
