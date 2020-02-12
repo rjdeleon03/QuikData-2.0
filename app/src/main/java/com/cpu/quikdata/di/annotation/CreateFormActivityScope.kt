f3fa09c315ff3b141518ff7e52b7a7c8bb597f35
rjdeleon03 <rjdeleon@up.edu.ph>
2020-02-12 13:03:16 +0800

    Create main activity and create form scope

---
+++ app/src/main/java/com/cpu/quikdata/di/annotation/CreateFormActivityScope.kt
@@ -0,0 +1,11 @@
+package com.cpu.quikdata.di.annotation
+
+import dagger.MapKey
+
+@MustBeDocumented
+@kotlin.annotation.Target(AnnotationTarget.FUNCTION,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.FIELD)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class CreateFormActivityScope()
