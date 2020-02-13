c29faed87671b14b780218ddff2a3df64d5d687b
rjdeleon03 <rjdeleon@up.edu.ph>
2020-02-13 09:16:55 +0800

    Pass DB in baseline data repository constructor

--- app/src/main/java/com/cpu/quikdata/di/module/createform/sections/BaselineDataModule.kt
+++ app/src/main/java/com/cpu/quikdata/di/module/createform/sections/BaselineDataModule.kt
@@ -1,7 +1,7 @@
 package com.cpu.quikdata.di.module.createform.sections
 
-import android.app.Application
 import androidx.lifecycle.ViewModel
+import com.cpu.quikdata.data.AppDatabase
 import com.cpu.quikdata.di.annotation.FormIdQualifier
 import com.cpu.quikdata.di.annotation.ViewModelKey
 import com.cpu.quikdata.feature.createform.formdetailsandbaseline.baselinedata.BaselineDataRepository
@@ -22,9 +22,9 @@ abstract class BaselineDataModule {
     companion object {
 
         @Provides
-        fun provideBaselineDataRepository(application: Application, @FormIdQualifier formId: String)
+        fun provideBaselineDataRepository(database: AppDatabase, @FormIdQualifier formId: String)
                 : BaselineDataRepository {
-            return BaselineDataRepository(application, formId)
+            return BaselineDataRepository(database, formId)
         }
     }
 }
--- app/src/main/java/com/cpu/quikdata/feature/createform/formdetailsandbaseline/baselinedata/BaselineDataRepository.kt
+++ app/src/main/java/com/cpu/quikdata/feature/createform/formdetailsandbaseline/baselinedata/BaselineDataRepository.kt
@@ -1,15 +1,13 @@
 package com.cpu.quikdata.feature.createform.formdetailsandbaseline.baselinedata
 
-import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
-import com.cpu.quikdata.base.BaseRepository
+import com.cpu.quikdata.data.AppDatabase
 import com.cpu.quikdata.data.baselinedata.BaselineData
 import com.cpu.quikdata.data.prefilleddata.PrefilledData
 import com.cpu.quikdata.utils.runOnIoThread
 
-class BaselineDataRepository(application: Application, formId: String) :
-    BaseRepository<BaselineData>(application) {
+class BaselineDataRepository(private val mDatabase: AppDatabase, val formId: String) {
 
     private val mBaselineData = mDatabase.baselineDataDao().getByFormId(formId)
     private val mPrefilledData = MediatorLiveData<PrefilledData>()
@@ -20,7 +18,7 @@ class BaselineDataRepository(application: Application, formId: String) :
     val prefilledData: LiveData<PrefilledData>
         get() = mPrefilledData
 
-    override fun updateData(data: BaselineData) {
+    fun updateData(data: BaselineData) {
         runOnIoThread {
             val oldBaselineData = mBaselineData.value!!
             oldBaselineData.copyFrom(data)
