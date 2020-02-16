bb19a5ca6e0851a9cbcf26a7dd64ad061f54d0f7
rjdeleon <ricardopablo.deleon@gmail.com>
2020-02-16 10:52:23 +0800

    Add button in home to send emergency alert

--- app/src/main/java/com/cpu/quikdata/utils/NetworkUtils.kt
+++ app/src/main/java/com/cpu/quikdata/utils/NetworkUtils.kt
@@ -1,15 +1,30 @@
+@file:Suppress("DEPRECATION")
+
 package com.cpu.quikdata.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.NetworkInfo
+import android.os.Build
 import com.cpu.quikdata.R
 import com.cpu.quikdata.common.showToast
 
 fun isInternetAvailable(context: Context) : Boolean {
     val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-    return activeNetwork?.isConnected == true
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val nw = cm.activeNetwork ?: return false
+        val actNw = cm.getNetworkCapabilities(nw) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
+    } else {
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnected ?: false
+    }
 }
 
 fun isInternetAvailableThenToast(context: Context,
