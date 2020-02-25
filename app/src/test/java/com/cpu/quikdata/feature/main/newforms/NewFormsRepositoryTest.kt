219173046d0327eb4d93b0189b528565ec8c5702
rjdeleon <ricardopablo.deleon@gmail.com>
2020-02-25 17:01:13 +0800

    Add new forms repository and VM tests

---
+++ app/src/test/java/com/cpu/quikdata/feature/main/newforms/NewFormsRepositoryTest.kt
@@ -0,0 +1,35 @@
+package com.cpu.quikdata.feature.main.newforms
+
+import com.cpu.quikdata.feature.base.BaseCoroutineLiveDataTest
+import org.junit.Test
+
+class NewFormsRepositoryTest : BaseCoroutineLiveDataTest() {
+
+    override fun setupResources() {
+
+    }
+
+    @Test
+    fun getNewForms() {
+    }
+
+    @Test
+    fun getSaveResult() {
+    }
+
+    @Test
+    fun createNewForm() {
+    }
+
+    @Test
+    fun deleteForm() {
+    }
+
+    @Test
+    fun submitForm() {
+    }
+
+    @Test
+    fun cancelSubmission() {
+    }
+}
---
+++ app/src/test/java/com/cpu/quikdata/feature/main/newforms/NewFormsViewModelTest.kt
@@ -0,0 +1,34 @@
+package com.cpu.quikdata.feature.main.newforms
+
+import com.cpu.quikdata.feature.base.BaseCoroutineLiveDataTest
+import org.junit.Test
+
+class NewFormsViewModelTest : BaseCoroutineLiveDataTest() {
+    
+    override fun setupResources() {
+    }
+
+    @Test
+    fun getNewForms() {
+    }
+
+    @Test
+    fun getSaveResult() {
+    }
+
+    @Test
+    fun createNewForm() {
+    }
+
+    @Test
+    fun deleteForm() {
+    }
+
+    @Test
+    fun submitForm() {
+    }
+
+    @Test
+    fun cancelSubmission() {
+    }
+}
