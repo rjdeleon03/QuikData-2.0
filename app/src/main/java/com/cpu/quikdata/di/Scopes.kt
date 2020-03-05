fd41a82b06e081d4540bbb123da74469c892f5fc
Ricardo Pablo De Leon <rjdeleon@up.edu.ph>
2020-03-05 22:14:34 +0800

    Add scopes

---
+++ app/src/main/java/com/cpu/quikdata/di/Scopes.kt
@@ -0,0 +1,13 @@
+package com.cpu.quikdata.di
+
+import javax.inject.Scope
+
+@Scope
+@MustBeDocumented
+@Retention(value = AnnotationRetention.RUNTIME)
+annotation class ActivityScope
+
+@Scope
+@MustBeDocumented
+@Retention(value = AnnotationRetention.RUNTIME)
+annotation class FragmentScope
--- app/src/main/java/com/cpu/quikdata/di/component/main/PrefilledInfoComponent.kt
+++ app/src/main/java/com/cpu/quikdata/di/component/main/PrefilledInfoComponent.kt
@@ -1,9 +1,11 @@
 package com.cpu.quikdata.di.component.main
 
+import com.cpu.quikdata.di.FragmentScope
 import com.cpu.quikdata.di.app.module.PrefilledInfoModule
 import com.cpu.quikdata.feature.main.prefilledinfo.PrefilledInfoFragment
 import dagger.Subcomponent
 
+@FragmentScope
 @Subcomponent(modules = [PrefilledInfoModule::class])
 interface PrefilledInfoComponent {
 
